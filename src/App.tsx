58ef01f4f31e016421aaafcbf23b13ea42ee7337
Arlindo jose <arlindojosboa@gmail.com>
2020-09-15 12:44:15 +0200

    facebook

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,9 @@ import React from 'react';
 import Layout from './Layout';
 
 function App() {
-  const user = 'j';
-  if(!user){
-    return (<h1>Login</h1>)
-  } else {
-    return (<Layout />)
-  }
+  return (
+    <Layout />
+  )
 }
 
 export default App;
